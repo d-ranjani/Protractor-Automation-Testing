6163ed5847b1b6070074eeebcf007867eccac1c1
ranjini <d.ranjani@mcwaneservices.com>
2015-07-07 12:53:18 +0530

    Second Commit

--- spec.js
+++ spec.js
@@ -2,30 +2,32 @@
 //spec.js
 browser.ignoreSynchronization = true;
 
-// To open Google.Com page
+//Open Google.Com page
 describe('To open google browser and search the text "Thought For The Day"', function() {
-  it('To click the link Thought for the day', function() {
+  it('To click the link Thought For The Day', function() {
     browser.driver.get('http://google.com/');
 
    
-//To Maximize the Window   
+//Maximize the Window   
     browser.driver.manage().window().maximize();
 
-//To enter search text "Thought for the day"
+//Enter search text "Thought For The Day"
     element(by.id('lst-ib')).sendKeys("Thought for the Day");
     browser.sleep(1000);
     element(by.name('btnG')).click();
 
   
-//To Read the Title of the Page				
+//click the LinkText "Thought For The Day"				
     element(by.linkText('Thought For The Day')).click();
     
 
-//To Read the Title of the Page
+//Read the Title of the Page
     var titlePromise = browser.getTitle();
     titlePromise.then(function(text){
       console.log("Title of the Page", text);
-	browser.close();
+	
+//Tear down
+    browser.close();
     
 });
 });
